5e232f07d997f861d0c6ec3ec81a7e7b050645b1
Gliga Dumitru <gliga_dumitru@yahoo.com>
2020-02-11 15:49:38 +0200

    Updates

---
+++ precache-manifest.2eddaf0aa9d24fea88cfcab80ffc4cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcf17d413a3eebaade121074ed318b0",
+    "url": "/shop-22/index.html"
+  },
+  {
+    "revision": "4d540c325048be16d343",
+    "url": "/shop-22/static/css/main.940ffe07.chunk.css"
+  },
+  {
+    "revision": "6fcccebabd52ce4b0a17",
+    "url": "/shop-22/static/js/2.48d1c262.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shop-22/static/js/2.48d1c262.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d540c325048be16d343",
+    "url": "/shop-22/static/js/main.da1c64c6.chunk.js"
+  },
+  {
+    "revision": "8ab1452c6a751d8b350d",
+    "url": "/shop-22/static/js/runtime-main.ef6db189.js"
+  }
+]);
